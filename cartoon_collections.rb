147bb922faef6064822e06743ba0b5914c364564
Jason Lewis <jasonedlewis@gmail.com>
2019-07-06 05:51:31 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,8 @@ end
 
 def long_planeteer_calls(array)# code an argument here
   # Your code here
-    long_calls = array.select{|call| call.length > 4}
+    long_calls = []
+    array.select{|call| long_calls << call.length > 4}
     long_calls
 end
 
