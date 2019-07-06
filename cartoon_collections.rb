46dc7bbf221c8751b4f866dc4e55e2060e965b2d
Jason Lewis <jasonedlewis@gmail.com>
2019-07-06 05:22:57 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,8 +3,10 @@ def roll_call_dwarves(array) # code an argument here
   array.each_with_index{|dwarf,index| puts "#{index+1}. #{dwarf}"}
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(array)# code an argument here
   # Your code here
+     changed = array.each{|name|  "#{name.capitalize"  }
+     changed
 end
 
 def long_planeteer_calls# code an argument here
