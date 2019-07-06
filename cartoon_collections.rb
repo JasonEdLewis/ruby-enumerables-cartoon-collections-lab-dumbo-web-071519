0e63ee6053ae4534baa96c9af100b5be800d9df3
Jason Lewis <jasonedlewis@gmail.com>
2019-07-06 05:30:39 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,8 +5,8 @@ end
 
 def summon_captain_planet(array)# code an argument here
   # Your code here
-     changed = array.each{|name| "#{name.capitalize}!"}
-     changed
+     return array.each{|name| "#{name.capitalize}!"}
+     
 end
 
 def long_planeteer_calls(array)# code an argument here
