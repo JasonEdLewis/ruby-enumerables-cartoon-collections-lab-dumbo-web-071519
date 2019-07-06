5dce193c3fe6b599210adbec841abdbabe061f50
Jason Lewis <jasonedlewis@gmail.com>
2019-07-06 05:53:21 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,9 +12,8 @@ end
 
 def long_planeteer_calls(array)# code an argument here
   # Your code here
-    long_calls = []
-    array.select{|call| call.length > 4 ? long_calls << call : long_calls}
-    long_calls
+    array.any?{|call| call.length > 4 ?}
+    
 end
 
 def find_the_cheese# code an argument here
